6554e4d3dcefea9946b9e721870f2a86ff691c6c
JamesPreston1993 <jamescpreston93@gmail.com>
2015-10-19 20:01:28 +0100

    Added new DTC views - Page and icons

--- VSDA/App.xaml.cs
+++ VSDA/App.xaml.cs
@@ -54,7 +54,7 @@ namespace VSDA
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                this.DebugSettings.EnableFrameRateCounter = false;
             }
 #endif
 
---
+++ VSDA/UI/DTCPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using VSDA.Communication;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace VSDA.UI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DTCPage : Page
+    {
+        private IDtcModule module;
+
+        public DTCPage(IDtcModule module)
+        {
+            this.module = module;
+            this.InitializeComponent();
+            this.DisplayCodes();
+        }
+
+        private async void DisplayCodes()
+        {
+            this.StoredCodesGrid.Children.Clear();
+            this.PendingCodesGrid.Children.Clear();
+            this.PermanentCodesGrid.Children.Clear();
+
+            IList<ICode> storedCodes = await this.module.GetCurrentCodes();
+            foreach(ICode code in storedCodes)
+            {
+                DTCView view = new DTCView(code);
+                view.Height = 80;
+                this.StoredCodesGrid.Children.Add(view);
+            }
+
+            IList<ICode> pendingCodes = await this.module.GetPendingCodes();
+            foreach (ICode code in pendingCodes)
+            {
+                DTCView view = new DTCView(code);
+                view.Height = 80;
+                this.PendingCodesGrid.Children.Add(view);
+            }
+            
+            IList<ICode> permanentCodes = await this.module.GetPermanentCodes();
+            foreach (ICode code in permanentCodes)
+            {
+                DTCView view = new DTCView(code);
+                view.Height = 80;
+                this.PermanentCodesGrid.Children.Add(view);
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.DisplayCodes();
+        }
+    }
+}
