383be52654178033b3f3ec4ed9d142150cfebec8
JamesPreston1993 <jamescpreston93@gmail.com>
2016-01-11 16:36:40 +0000

    Temporary - Host initializes to the simulation connection layer

--- Code/VSDA/Communication/Host.cs
+++ Code/VSDA/Communication/Host.cs
@@ -4,24 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using VSDA.Connection;
+
 
 namespace VSDA.Communication
 {
     public class Host : IHost
     {
         public List<IModule> Modules { get; private set; }
+        public IModule CurrentModule { get; set; }
 
         public Host(List<IModule> modules)
         {
             this.Modules = modules;
-            this.CurrentModule = modules[0];      
-        }
-    
-        public IModule CurrentModule { get; set; }
+            this.CurrentModule = modules[0];
+            ConnectionManager.Instance.Initialize();
+        }            
 
         public void Initialize()
         {
-            this.CurrentModule.Initialize();               
+            //this.CurrentModule.Initialize();               
         }
     }
 }
