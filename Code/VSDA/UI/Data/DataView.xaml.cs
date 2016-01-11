3f4492d04747607ad1974cd0e29289e46dbd694e
JamesPreston1993 <jamescpreston93@gmail.com>
2016-01-11 09:15:20 +0000

    Added Data List UI

---
+++ Code/VSDA/UI/Data/DataView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using VSDA.Communication.Data;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace VSDA.UI
+{
+    public sealed partial class DataView : UserControl
+    {
+        private IDataListViewModel pid;
+
+        public DataView(IDataListViewModel pid)
+        {
+            this.pid = pid;
+            this.DataContext = this.pid;
+            this.InitializeComponent();            
+        }     
+    }
+}
