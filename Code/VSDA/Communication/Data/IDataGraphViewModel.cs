97c565e47b07e0fa7769557ffd4b1a4304ea3b4e
JamesPreston1993 <jamescpreston93@gmail.com>
2016-01-11 09:07:27 +0000

    Added Graph and List ViewModels

---
+++ Code/VSDA/Communication/Data/IDataGraphViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+
+namespace VSDA.Communication.Data
+{
+    public interface IDataGraphViewModel : IDataViewModel
+    {
+        PointCollection Points { get; }
+
+        double CursorPosition { get; }
+
+        double MaxPossibleValue { get; }
+
+        double MinPossibleValue { get; }        
+    }
+}
---
+++ Code/VSDA/Communication/Data/IDataViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSDA.Communication;
+
+namespace VSDA.Communication.Data
+{
+    public interface IDataViewModel : INotifyPropertyChanged
+    {
+        IPid PidModel { get; }
+
+        string PidName { get; }        
+
+        int CurrentSample { get; set; }        
+
+        IList<string> DataItems { get; }
+
+        void StepBack();
+
+        void StepForward();
+
+        void SkipToStart();
+
+        void SkipToEnd();
+    }
+}
