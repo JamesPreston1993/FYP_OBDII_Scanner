2b74fc3a04755842087da5565d00d0e79f6e5877
JamesPreston1993 <jamescpreston93@gmail.com>
2016-01-12 12:59:17 +0000

    IModule now supports MVVM

--- Code/VSDA/Communication/Interfaces/IModule.cs
+++ Code/VSDA/Communication/Interfaces/IModule.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VSDA.Communication
 {
-    public interface IModule
+    public interface IModule : INotifyPropertyChanged
     {
         string Name { get; }        
 
         Task<bool> Initialize();
-
-        void Notify();
     }
 }
