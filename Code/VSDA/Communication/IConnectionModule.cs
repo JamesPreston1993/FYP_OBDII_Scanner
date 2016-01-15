b17d3e88b33fa20a928f7b72fd0a54b184aa7480
JamesPreston1993 <jamescpreston93@gmail.com>
2016-01-15 20:56:15 +0000

    Added Bluetooth connection module

---
+++ Code/VSDA/Communication/IConnectionModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+
+namespace VSDA.Communication
+{
+    public interface IConnectionModule : IModule
+    {
+        DeviceInformationCollection Devices { get; }
+
+        Task<bool> Connect(DeviceInformation device);
+    }
+}
