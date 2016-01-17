12dd50739cf616e4235f1388c224851bfe65a5c3
JamesPreston1993 <jamescpreston93@gmail.com>
2016-01-17 20:48:09 +0000

    Moved RelayCommand to VSDACore

---
+++ Code/VSDACore/Modules/Base/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace VSDACore.Modules.Base
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action action;
+        private Func<bool> canExecute;
+
+        public RelayCommand(Action action)
+        {
+            this.action = action;
+            this.canExecute = null;
+        }
+
+        public RelayCommand(Action action, Func<bool> canExecute)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExecute != null)
+                return canExecute();
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action.Invoke();
+        }
+    }
+}
