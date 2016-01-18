e55f2468cf34601a10ad19cd06ef4ec80f487322
JamesPreston1993 <jamescpreston93@gmail.com>
2016-01-18 10:41:08 +0000

    Updated App.xaml.cs to use VSDACore namespaces and set current connection

--- Code/VSDA/App.xaml.cs
+++ Code/VSDA/App.xaml.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using VSDA.UI;
+using VSDACore.Host;
+using VSDACore.Modules.Base;
+using VSDACore.Modules.Codes;
+using VSDACore.Modules.Connection;
+using VSDACore.Modules.Data;
+using VSDACore.Connection;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using VSDA.Communication;
-using VSDA.Communication.Data;
-using VSDA.UI;
+using VSDA.Connection;
 
 namespace VSDA
 {
@@ -37,6 +33,9 @@ namespace VSDA
                 Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+
+            // Set ConnectionManager
+            ConnectionManager.Instance = new BluetoothDataConnection();
 
             List<IModule> modules = new List<IModule>();
             
