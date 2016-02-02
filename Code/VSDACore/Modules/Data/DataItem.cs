78e19b1038e5208dfa131acbe46a6e5a4b3af12a
JamesPreston1993 <jamescpreston93@gmail.com>
2016-02-02 09:40:43 +0000

    Added DataItem to be used for smart graphing

---
+++ Code/VSDACore/Modules/Data/DataItem.cs
@@ -0,0 +1,23 @@
+﻿namespace VSDACore.Modules.Data
+{
+    public class DataItem : IDataItem
+    {
+        public double Value { get; private set; }
+
+        public string StringValue { get; private set; }
+
+        public ValueType Type { get; set; }
+
+        public DataItem(string stringValue)
+        {
+            this.Value = double.NaN;
+            this.StringValue = stringValue;
+        }
+
+        public DataItem(double value, string stringValue)
+        {
+            this.Value = value;
+            this.StringValue = stringValue;
+        }
+    }
+}
---
+++ Code/VSDACore/Modules/Data/IDataItem.cs
@@ -0,0 +1,18 @@
+﻿namespace VSDACore.Modules.Data
+{
+    public interface IDataItem
+    {
+        double Value { get; }
+
+        string StringValue { get; }
+
+        ValueType Type { get; set; }
+    }
+
+    public enum ValueType
+    {
+        Normal,
+        Caution,
+        Danger
+    }
+}
