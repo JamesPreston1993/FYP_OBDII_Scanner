1209fbfd34bb693da2c671485d3dfa022ec638d8
JamesPreston1993 <jamescpreston93@gmail.com>
2016-03-01 20:44:52 +0000

    Added place holders for Home Module

---
+++ Code/VSDACore/Modules/Home/HomeModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSDACore.Modules.Base;
+
+namespace VSDACore.Modules.Home
+{
+    public class HomeModule : IHomeModule
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name { get; private set; }
+
+        public IList<IHelpItem> HelpItems { get; private set; }
+
+        public async Task<bool> Initialize()
+        {
+            return true;
+        }
+
+        public async Task<bool> Shutdown()
+        {
+            return true;
+        }
+    }
+}
---
+++ Code/VSDACore/Modules/Home/IHomeModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSDACore.Modules.Base;
+
+namespace VSDACore.Modules.Home
+{
+    public interface IHomeModule : IModule
+    {
+    }
+}
