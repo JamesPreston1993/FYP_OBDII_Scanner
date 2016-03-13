62c8cbeec22d699d8e0acfaf9e7e1163ecd25a4d
JamesPreston1993 <jamescpreston93@gmail.com>
2016-03-13 21:48:49 +0000

    ListView now changes colour based on value type

--- Code/VSDA/UI/Data/DataView.xaml.cs
+++ Code/VSDA/UI/Data/DataView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using VSDACore.Modules.Data;
+﻿using System.ComponentModel;
+using VSDACore.Modules.Data;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -12,8 +14,32 @@ namespace VSDA.UI
         public DataView(IDataListViewModel pid)
         {
             this.pid = pid;
-            this.DataContext = this.pid;
-            this.InitializeComponent();            
-        }     
+            this.DataContext = this.pid;            
+            this.InitializeComponent();
+            this.pid.PropertyChanged += this.RaiseViewModelPropertyChanged;
+        }
+        
+        private void RaiseViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == "CurrentValue")
+            {                
+                switch(this.pid.DataItems[this.pid.CurrentSample].Type)
+                {                    
+                    case ValueType.Caution:
+                        this.PidName.Foreground = (App.Current.Resources["GraphPlotColorCaution"] as SolidColorBrush);
+                        this.PidValue.Foreground = (App.Current.Resources["GraphPlotColorCaution"] as SolidColorBrush);
+                        break;
+                    case ValueType.Danger:
+                        this.PidName.Foreground = (App.Current.Resources["GraphPlotColorDanger"] as SolidColorBrush);
+                        this.PidValue.Foreground = (App.Current.Resources["GraphPlotColorDanger"] as SolidColorBrush);
+                        break;
+                    case ValueType.Normal:
+                    case ValueType.Default:
+                        this.PidName.Foreground = (App.Current.Resources["ModulePageButtonForeground"] as SolidColorBrush);
+                        this.PidValue.Foreground = (App.Current.Resources["ModulePageButtonForeground"] as SolidColorBrush);
+                        break;
+                }
+            }
+        }
     }
 }
