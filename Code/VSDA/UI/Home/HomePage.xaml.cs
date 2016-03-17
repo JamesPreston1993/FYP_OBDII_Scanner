5829ef1f3f63b245b63166ec79b5fcdc8eca7e24
JamesPreston1993 <jamescpreston93@gmail.com>
2016-03-17 18:43:20 +0000

    Added HomePage UI

---
+++ Code/VSDA/UI/Home/HomePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using VSDACore.Modules.Base;
+using VSDACore.Modules.Home;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace VSDA.UI.Home
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        private IHomeModuleViewModel module;
+
+        public HomePage(IHomeModuleViewModel module)
+        {
+            this.module = module;
+            this.InitializeComponent();
+
+            this.ModuleHintsGrid.Children.Clear();
+            foreach (IHelpItem moduleHint in this.module.HelpItems)
+            {
+                this.ModuleHintsGrid.Children.Add(new ModuleHintView(moduleHint));
+                this.ModuleHintsGrid.Children.Add(new TextBlock() { Height = 10 });
+            }
+        }
+    }
+}
