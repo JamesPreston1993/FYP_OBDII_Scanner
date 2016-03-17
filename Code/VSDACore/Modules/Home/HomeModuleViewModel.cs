ba20224631f33b23e83eebc8edafb1cce0f6f5c2
JamesPreston1993 <jamescpreston93@gmail.com>
2016-03-17 18:36:15 +0000

    Added Home Module ViewModel

---
+++ Code/VSDACore/Modules/Home/HomeModuleViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using VSDACore.Modules.Base;
+
+namespace VSDACore.Modules.Home
+{
+    public class HomeModuleViewModel : IHomeModuleViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name { get; private set; }
+
+        public IList<IHelpItem> HelpItems { get; private set; }
+        
+        public IModule ModuleModel { get; set; }
+
+        public HomeModuleViewModel(IHomeModule module)
+        {
+            this.ModuleModel = module;
+            this.Name = module.Name;
+            this.HelpItems = module.HelpItems;
+        }
+
+        public async Task<bool> InitializeModule()
+        {
+            await this.ModuleModel.Initialize();
+            return true;
+        }
+
+        public string FormatForEmail()
+        {
+            return string.Empty;
+        }
+    }
+}
